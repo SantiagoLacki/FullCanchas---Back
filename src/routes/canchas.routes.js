2d0ba0d2de04d93412281460259cae62f568f8b1
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-10-01 15:30:16 -0300

    agregue la verificar del jsonwebtoken a las rutas de crear, borrar y editar canchas

--- src/routes/canchas.routes.js
+++ src/routes/canchas.routes.js
@@ -7,14 +7,18 @@ import {
   leerCanchasID,
 } from "../controllers/canchas.controllers.js";
 import validarCanchas from "../middleware/validarCanchas.js";
+import verificarJWT from "../middleware/verificarJWT.js";
 
 const router = Router();
 
-router.route("/").get(leerCanchas).post(validarCanchas, crearCancha);
+router
+  .route("/")
+  .get(leerCanchas)
+  .post([verificarJWT, validarCanchas], crearCancha);
 router
   .route("/:id")
   .get(leerCanchasID)
-  .delete(borrarCancha)
-  .put(validarCanchas, editarCancha);
+  .delete(verificarJWT, borrarCancha)
+  .put([verificarJWT, validarCanchas], editarCancha);
 
 export default router;
