1c06521ae86701ecd2d87de0a5a105607ffe326c
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-10-02 16:34:58 -0300

    agregue la validaicon de jsonwebtoken a los endpoint de las reservas

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -1,5 +1,20 @@
 import { Router } from "express";
+import {
+  borrarReserva,
+  crearReservas,
+  editarReserva,
+  leerReservas,
+  leerReservasPorID,
+} from "../controllers/reservas,controllers.js";
+import verificarJWT from "../middleware/verificarJWT.js";
 
 const router = Router();
+
+router.route("/").get(leerReservas).post(verificarJWT, crearReservas);
+router
+  .route("/:d")
+  .get(leerReservasPorID)
+  .delete(verificarJWT, borrarReserva)
+  .put(verificarJWT, editarReserva);
 
 export default router;
