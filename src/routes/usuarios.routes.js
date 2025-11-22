196ea41e9e31fece5224c97b9c9fe1c4a845a1a0
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-11-22 17:49:13 -0300

    elimine por completo el middleware isAdminOrStaff, ya no se usan secret_key

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -8,22 +8,18 @@ import {
   login,
   usuariosPaginacion,
 } from "../controllers/usuarios.controllers.js";
-import { isAdminOrStaff } from "../middleware/validarAdminStaff.js";
 import validarUsuarios from "../middleware/validarUsuarios.js";
 import verificarJWT from "../middleware/verificarJWT.js";
 
 const router = Router();
 
-router
-  .route("/")
-  .get(leerUsuarios)
-  .post([validarUsuarios, isAdminOrStaff], crearUsuario);
+router.route("/").get(leerUsuarios).post(validarUsuarios, crearUsuario);
 router.route("/paginado").get(usuariosPaginacion);
 router
   .route("/:id")
   .get(leerUsuariosPorID)
   .delete(verificarJWT, borrarUsuario)
-  .put([verificarJWT, validarUsuarios, isAdminOrStaff], editarUsuario);
+  .put([verificarJWT, validarUsuarios], editarUsuario);
 router.route("/login").post(login);
 
 export default router;
