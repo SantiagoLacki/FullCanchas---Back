3751d0b7eb7a1f496c6b04ab78ab95b36dfa939b
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-11-22 17:54:03 -0300

    agregue un middleware para asignar roles

---
+++ src/middleware/asignarRol.js
@@ -0,0 +1,20 @@
+export const asignarRol = (req, res, next) => {
+  const { rol } = req.body;
+  if (!req.usuario) {
+    req.rolAsignado = "user";
+    return next();
+  }
+
+  if (req.usuario.rol === "admin") {
+    if (rol === "empleado") {
+      req.rolAsignado = "empleado";
+      return next();
+    }
+    return res.status(403).json({
+      mensaje: "Un admin solo puede crear empleados",
+    });
+  }
+
+  req.rolAsignado = "user";
+  next();
+};
