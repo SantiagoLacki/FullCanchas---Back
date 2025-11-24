c1304d0b50a792109cfd09d27a11553688b1351c
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-11-23 21:26:13 -0300

    modifico los controladore de crear y borrar usuarios

--- src/middleware/asignarRol.js
+++ src/middleware/asignarRol.js
@@ -1,22 +1,27 @@
 const asignarRol = (req, res, next) => {
   const { rol } = req.body;
   if (!req.usuario) {
-    req.rolAsignado = "user";
+    req.rolAsignado = rol ?? "user";
+    return next();
+  }
+
+  if (req.usuario.rol === "superAdmin") {
+    req.rolAsignado = rol ?? "user";
     return next();
   }
 
   if (req.usuario.rol === "admin") {
-    if (rol === "empleado") {
-      req.rolAsignado = "empleado";
+    if (rol === "empleado" || rol === "user") {
+      req.rolAsignado = rol;
       return next();
     }
     return res.status(403).json({
-      mensaje: "Un admin solo puede crear empleados",
+      mensaje: "Un admin solo puede crear empleados o usuarios comunes",
     });
   }
-
-  req.rolAsignado = "user";
-  next();
+  return res.status(403).json({
+    mensaje: "No tienes permisos para crear usuarios",
+  });
 };
 
 export default asignarRol;
